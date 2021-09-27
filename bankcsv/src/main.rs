8550fd887c06f340b6aa385ac22e431d0e3d8d21
Ron Georgia <rong1611@gmail.com>
2021-09-27 07:48:39 -0400

    Update main.rs

--- bankcsv/src/main.rs
+++ bankcsv/src/main.rs
@@ -1,6 +1,7 @@
 mod model;
 
 fn main() {
+    // need OS agnostic path to file
     let data_path = "../data" ;
     let checking_file = "Checking1.csv";
     let credit_card_file = "CreditCard3.csv" ;
