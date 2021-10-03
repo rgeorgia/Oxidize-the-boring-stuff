9a3cac961166511cb51572d3ad125c00399c2aaa
Ron Georgia <rong1611@gmail.com>
2021-10-03 07:57:58 -0400

    Update main

--- bankcsv/src/main.rs
+++ bankcsv/src/main.rs
@@ -1,10 +1,45 @@
-mod model;
+use std::fs::File;
+use std::io;
+use std::io::BufReader ;
+use std::io::prelude::* ;
+use regex::{Regex};
+use clap::{App, Arg};
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            None => (),
+            Some(_) => println!("{}", line),
+        }
+    }
+}
 
 fn main() {
-    let data_path = "../data" ;
-    let checking_file = "Checking1.csv";
-    let credit_card_file = "CreditCard3.csv" ;
+     let args = App::new("bankcsv")
+         .version("0.1")
+         .about("Searches for patterns")
+         .arg(Arg::with_name("pattern")
+             .help("Pattern to search for")
+             .takes_value(true)
+             .required(true))
+         .arg(Arg::with_name("input")
+             .help("file to search")
+             .takes_value(true)
+             .required(false))
+         .get_matches();
 
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+    let input = args.value_of("input").unwrap_or("-");
 
-    println!("Hello, world!");
+    if input == "-" {
+        let stdin = io::stdin() ;
+        let reader = stdin.lock();
+        process_lines(reader, re) ;
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader,re) ;
+    }
 }
