fd7c84f9171e6c128ab1c9b73cf18dbbc0bc367c
Ron Georgia <rong1611@gmail.com>
2021-11-06 08:51:44 -0400

    update main and cargo.toml

--- bankcsv/src/main.rs
+++ bankcsv/src/main.rs
@@ -1,40 +1,15 @@
 use std::fs::File;
+use std::fs::Path ;
 use std::io;
 use std::io::BufReader ;
 use std::io::prelude::* ;
-use regex::{Regex};
-use clap::{App, Arg};
+use std::path::PathBuf;
+use csv;
 
-fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            None => (),
-            Some(_) => println!("{}", line),
-        }
-    }
-}
+
 
 fn main() {
-     let args = App::new("bankcsv")
-         .version("0.1")
-         .about("Searches for patterns")
-         .arg(Arg::with_name("input")
-             .help("file to search")
-             .takes_value(true)
-             .required(false))
-         .get_matches();
+    let chk_file = PathBuf("../data/Checking1.csv");
 
 
-    let input = args.value_of("input").unwrap_or("-");
-
-    if input == "-" {
-        let stdin = io::stdin() ;
-        let reader = stdin.lock();
-        process_lines(reader, re) ;
-    } else {
-        let f = File::open(input).unwrap();
-        let reader = BufReader::new(f);
-        process_lines(reader,re) ;
-    }
 }
