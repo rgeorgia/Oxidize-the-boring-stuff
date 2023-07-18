2060a2a6ad59a23476af384df865bb175435c67f
Ron Georgia <rong1611@gmail.com>
2023-07-18 19:24:06 -0400

    Updated main with reader loop

--- bankcsv/src/main.rs
+++ bankcsv/src/main.rs
@@ -1,5 +1,20 @@
 mod bank_statement;
+use std::error::Error;
+use std::process;
+use csv;
 
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    for result in reader.records(){
+        let record = result?;
+        println!("{:?}", record)
+    }
+
+    Ok(())
+}
 fn main() {
-    println!("Bank csv");
+    if let Err(e) = read_from_file("./data/checking_with_header.csv") {
+        println!("Error reading file: {}", e);
+        process::exit(1);
+    }
 }
